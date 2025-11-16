8afe2582dba9b669b85b69f01ef87241062d2909
anhntp11 <lazyfoxcrazy1123@gmail.com>
2025-11-16 19:41:22 +0100

    entity_Livre

--- backend/src/main/java/fr/orleans/m1s1miage/group4/backend/model/entity/Livre.java
+++ backend/src/main/java/fr/orleans/m1s1miage/group4/backend/model/entity/Livre.java
@@ -2,13 +2,62 @@ package fr.orleans.m1s1miage.group4.backend.model.entity;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name = "Livre")
+
 public class Livre {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String test;
+    private Long idLivre;
+    private String titre;
+    private String langue;
+    private String edition;
+    private String nomAuteur;
+    private LocalDateTime tempsCreer;
+    private LocalDateTime tempsMiseAJour;
+
+    public Long getIdLivre() {
+        return idLivre;
+    }
+    public void setIdLivre(Long idLivre) {
+        this.idLivre = idLivre;
+    }
+    public String getTitre() {
+        return titre;
+    }
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+    public String getLangue() {
+        return langue;
+    }
+    public void setLangue(String langue) {
+        this.langue = langue;
+    }
+    public String getEditon() {
+        return edition;
+    }
+    public void setEditon(String edition) {
+        this.edition = edition;
+    }
+    public String getNomAuteur() {
+        this.nomAuteur = nomAuteur;
+    }
+    public LocalDateTime getTempsCreer() {
+        return tempsCreer;
+    }
+    public LocalDateTime setTempsCreer(LocalDateTime tempsCreer) {
+        this.tempsCreer = tempsCreer;
+    }
+    public LocalDateTime getTempsMiseAJour() {
+        return tempsMiseAJour;
+    }
+    public void setTempsMiseAJour(LocalDateTime tempsMiseAJour) {
+        this.tempsMiseAJour = tempsMiseAJour;
+    }
+
     @Column(nullable = false)
     
 }
