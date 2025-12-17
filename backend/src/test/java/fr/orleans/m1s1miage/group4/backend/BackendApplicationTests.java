6a30b9726962ddd5f38ee0d1e1ebf174523c7579
Vu The Duc <the-duc.vu@etu.univ-orleans.fr>
2025-12-17 16:22:11 +0100

    modified all the tests

--- backend/src/test/java/fr/orleans/m1s1miage/group4/backend/BackendApplicationTests.java
+++ backend/src/test/java/fr/orleans/m1s1miage/group4/backend/BackendApplicationTests.java
@@ -10,7 +10,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @ActiveProfiles("test")
 @SpringBootTest(properties = {
-        // H2 in-memory, giữ DB sống trong suốt vòng đời JVM
         "spring.datasource.url=jdbc:h2:mem:monapp_db;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
         "spring.datasource.driverClassName=org.h2.Driver",
         "spring.datasource.username=root",
